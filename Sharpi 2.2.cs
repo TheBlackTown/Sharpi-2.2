ef3c866ae39bde5254777638168905e5599d570b
TheBlackTown <70762240+TheBlackTown@users.noreply.github.com>
2020-09-12 11:27:41 +0300

    Add files via upload

---
+++ Sharpi 2.2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortString
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] arr = s.Split(' ');
+            Array.Sort(arr);
+            s = string.Join(" ", arr);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
